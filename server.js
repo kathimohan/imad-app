e56acd4eb065164a25848c1a091776faf556ae66
kathimohan <36533398+kathimohan@users.noreply.github.com>
2018-02-19 09:37:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/article1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
+app.get('/article2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
